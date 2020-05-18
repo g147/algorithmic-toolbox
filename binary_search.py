afbd17a5ae863c7915bab2498e5bdbf031193b45
BEAST GLATISANT <beast@glatisant.localhost>
2020-05-18 09:26:21 +0530

    added more

---
+++ binary_search.py
@@ -0,0 +1,30 @@
+# Uses python3
+import sys
+
+def binary_search(arr, x):
+    l, r = 0, len(arr)-1
+    while l <= r: 
+        mid = l + (r - l) // 2
+        if arr[mid] == x: 
+            return mid 
+        elif arr[mid] < x: 
+            l = mid + 1
+        else: 
+            r = mid - 1
+    return -1
+
+def linear_search(a, x):
+    for i in range(len(a)):
+        if a[i] == x:
+            return i
+    return -1
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
+    n = data[0]
+    m = data[n + 1]
+    a = data[1 : n + 1]
+    for x in data[n + 2:]:
+        # replace with the call to binary_search when implemented
+        print(binary_search(a, x), end = ' ')
